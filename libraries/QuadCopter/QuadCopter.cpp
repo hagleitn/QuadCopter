e4c3d22172702bd4e94ced97ae4f07af50a3563a
barbermot <hagleitn@Administrators-MacBook-Pro.local>
2011-06-22 00:44:19 -0700

    tabs... again

--- libraries/QuadCopter/QuadCopter.cpp
+++ libraries/QuadCopter/QuadCopter.cpp
@@ -1,81 +1,81 @@
 #include <QuadCopter.h>
 
 void QuadCopter::arm() {
-        // GAUI 330X good startup sound
-	// beep-beep-beep[increse pitch] (3S)beep-beep-beep[quick] beep[LOW]
-	stop(); // neutral state
-	adjustGain(0); // initialize gain
-        throttle(MIN_SPEED); //setting required to allow the 330X to start
+    // GAUI 330X good startup sound
+    // beep-beep-beep[increse pitch] (3S)beep-beep-beep[quick] beep[LOW]
+    stop(); // neutral state
+    adjustGain(0); // initialize gain
+    throttle(MIN_SPEED); //setting required to allow the 330X to start
 }
 
-QuadCopter::QuadCopter(	
-	int aileronPin, 
-	int rudderPin, 
-	int throttlePin, 
-	int elevatorPin, 
-	int gainPin) 
+QuadCopter::QuadCopter( 
+    int aileronPin, 
+    int rudderPin, 
+    int throttlePin, 
+    int elevatorPin, 
+    int gainPin) 
 {
-	pins[0] = elevatorPin;   // Red 
-	pins[1] = aileronPin;    // White 
-	pins[2] = throttlePin;   // Orange 
-	pins[3] = rudderPin;     // Yellow 
-	this->gainPin = gainPin; // Green (Gain/Gear) 
-	speed[0] = speed[1] = speed[2] = speed[3] = 0;
+    pins[0] = elevatorPin;   // Red 
+    pins[1] = aileronPin;    // White 
+    pins[2] = throttlePin;   // Orange 
+    pins[3] = rudderPin;     // Yellow 
+    this->gainPin = gainPin; // Green (Gain/Gear) 
+    speed[0] = speed[1] = speed[2] = speed[3] = 0;
 }
 
 
 void QuadCopter::attach() {
-	for (int i = 0; i < DEGREES_OF_FREEDOM; ++i) {
-		servos[i].attach(pins[i]);
-	}
-	gain.attach(gainPin);
+    for (int i = 0; i < DEGREES_OF_FREEDOM; ++i) {
+        servos[i].attach(pins[i]);
+    }
+    gain.attach(gainPin);
 }
 
 void QuadCopter::init() {
-	attach();
-	arm();
+    attach();
+    arm();
 }
 
 void QuadCopter::adjustGain(int gainVal) {
-	gain.write(map(gainVal, MIN_SPEED, MAX_SPEED, 0, 180));
+    gain.write(map(gainVal, MIN_SPEED, MAX_SPEED, 0, 180));
 }
 
 void QuadCopter::stop(Direction d) {
-	move(d,STOP_SPEED);
+    move(d,STOP_SPEED);
 }
 
 void QuadCopter::stop() {
-	for (int i = 0; i < DEGREES_OF_FREEDOM; ++i) {
-		stop((Direction)i);
-	}
+    for (int i = 0; i < DEGREES_OF_FREEDOM; ++i) {
+        stop((Direction)i);
+    }
 }
 
 void QuadCopter::move(Direction d, int speed) {
-	if (speed > MAX_SPEED) {
-		speed = MAX_SPEED;
-	} else if (speed < MIN_SPEED) {
-		speed = MIN_SPEED;
-	}
-	
-	Servo &s = this->servos[d];
-	
-	speed = map(speed ,MIN_SPEED, MAX_SPEED, 0, 180);	
-	
-	if (speed != s.read()) { 
-		s.write(speed);
-		this->speed[d] = speed;	
-	}
+    if (speed > MAX_SPEED) {
+        speed = MAX_SPEED;
+    } else if (speed < MIN_SPEED) {
+        speed = MIN_SPEED;
+    }
+    
+    Servo &s = this->servos[d];
+    
+    speed = map(speed ,MIN_SPEED, MAX_SPEED, 0, 180);   
+    
+    if (speed != s.read()) { 
+        s.write(speed);
+        this->speed[d] = speed; 
+    }
 }
 
 void QuadCopter::move(int speeds[]) {
-	for (int i = 0; i < DEGREES_OF_FREEDOM; ++i) {
-		move((Direction)i, speeds[i]);
-	}
+    for (int i = 0; i < DEGREES_OF_FREEDOM; ++i) {
+        move((Direction)i, speeds[i]);
+    }
 }
 
 void QuadCopter::move(int x, int y, int z, int r) {
-	move(LONGITUDINAL, x); 
-	move(LATERAL, y);
-	move(VERTICAL, z);
-	move(ROTATIONAL, r);
+    move(LONGITUDINAL, x); 
+    move(LATERAL, y);
+    move(VERTICAL, z);
+    move(ROTATIONAL, r);
 }
--- libraries/QuadCopter/QuadCopter.h
+++ libraries/QuadCopter/QuadCopter.h
@@ -6,49 +6,49 @@
 
 class QuadCopter {
 public:
-	
-	typedef enum {LONGITUDINAL=0, LATERAL=1, VERTICAL=2, ROTATIONAL=3} Direction;
-	/*
-		Colour map for GU-344 gyro pins (available with GAUI 330X)
-		aileronPin;    // White 
-		rudderPin;     // Yellow 
-		throttlePin;   // Orange 
-		elevatorPin;   // Red 
-		gainPin;       // Green (Gain/Gear) 
-	*/
-	QuadCopter(int aileronPin, int rudderPin, int throttlePin, int elevatorPin, int gainPin);
-	~QuadCopter() {}
-	void init();
-	void move(int x, int y, int z, int r);
-	void move(int[]);
-	void move(Direction,int);
-	void stop(Direction);
-	void stop();
-	int read(Direction d) {return speed[d];};
-	int (&read())[4] {return speed;};
-	void throttle(int speed) {move(VERTICAL,speed);}
-	void elevator(int speed) {move(LONGITUDINAL,speed);}
-	void aileron(int speed) {move(LATERAL,speed);}
-	void rudder(int speed) {move(ROTATIONAL,speed);}
-	void adjustGain(int);
-	
-	static const int DEGREES_OF_FREEDOM = 4;
-	static const int MIN_SPEED = -100;
-	static const int STOP_SPEED = 0;
-	static const int MAX_SPEED = 100;
+    
+    typedef enum {LONGITUDINAL=0, LATERAL=1, VERTICAL=2, ROTATIONAL=3} Direction;
+    /*
+        Colour map for GU-344 gyro pins (available with GAUI 330X)
+        aileronPin;    // White 
+        rudderPin;     // Yellow 
+        throttlePin;   // Orange 
+        elevatorPin;   // Red 
+        gainPin;       // Green (Gain/Gear) 
+    */
+    QuadCopter(int aileronPin, int rudderPin, int throttlePin, int elevatorPin, int gainPin);
+    ~QuadCopter() {}
+    void init();
+    void move(int x, int y, int z, int r);
+    void move(int[]);
+    void move(Direction,int);
+    void stop(Direction);
+    void stop();
+    int read(Direction d) {return speed[d];};
+    int (&read())[4] {return speed;};
+    void throttle(int speed) {move(VERTICAL,speed);}
+    void elevator(int speed) {move(LONGITUDINAL,speed);}
+    void aileron(int speed) {move(LATERAL,speed);}
+    void rudder(int speed) {move(ROTATIONAL,speed);}
+    void adjustGain(int);
+    
+    static const int DEGREES_OF_FREEDOM = 4;
+    static const int MIN_SPEED = -100;
+    static const int STOP_SPEED = 0;
+    static const int MAX_SPEED = 100;
 
 private:
-	
-	void arm();
-	void attach();
-	
-	Servo servos[DEGREES_OF_FREEDOM];
-	Servo gain;
-	
-	int speed[DEGREES_OF_FREEDOM];
-	
-	int pins[DEGREES_OF_FREEDOM];
-	int gainPin;
+    
+    void arm();
+    void attach();
+    
+    Servo servos[DEGREES_OF_FREEDOM];
+    Servo gain;
+    
+    int speed[DEGREES_OF_FREEDOM];
+    
+    int pins[DEGREES_OF_FREEDOM];
+    int gainPin;
 };
 
 #endif
