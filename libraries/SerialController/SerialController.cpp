76c6b941c26e637d5eb15c55ef3f6641456548fe
barbermot <hagleitn@Administrators-MacBook-Pro.local>
2011-07-15 02:39:27 -0700

    remove delay after sleep command

--- libraries/SerialController/SerialController.cpp
+++ libraries/SerialController/SerialController.cpp
@@ -10,7 +10,6 @@ void SerialController::executeCommand() {
     
     if (startSleep != 0) {
         if (millis()-startSleep < sleepTime) {
-            delay(100);
             return;
         } else {
             sleepTime = 0;
@@ -20,14 +19,14 @@ void SerialController::executeCommand() {
     
     const char *cmd = token.read();
     if (0 != cmd) {
-		if (cmd[0] == 'z' || cmd[0] == 'Z') {
-			int x = 0;
-			int ret = sscanf(&cmd[2],"%d",&x);
-			if (1 != ret) { parser.fail(); }
-			sleepTime = x;
-			startSleep = millis();
-		} else {
-			parser.doCmd(cmd);
-		}
+        if (cmd[0] == 'z' || cmd[0] == 'Z') {
+            int x = 0;
+            int ret = sscanf(&cmd[2],"%d",&x);
+            if (1 != ret) { parser.fail(); }
+            sleepTime = x;
+            startSleep = millis();
+        } else {
+            parser.doCmd(cmd);
+        }
     }
 }
